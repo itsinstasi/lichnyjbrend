5060dfdc7347c81b49b8f809f9cd88052e9a9002
tt <t@user.com>
2019-12-27 21:35:25 +0300

    Popup settings

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,6 +1,7 @@
 //var supportsTouch = ('ontouchstart' in document.documentElement);
 //console.log(supportsTouch);
 
+var canShowPopup = $('#js-stories-popup').length && popupSettings;
 $(document).ready(function(){
 	//grid
 	$('#js-grid').on('click', function(){
@@ -18,7 +19,14 @@ $(document).ready(function(){
 	});
 	
 	//stories popup
-	$('.js-stories-popup-open').on('click', function(){
+	$('.js-stories-popup-open').on('click', showPopup);
+
+        function showPopup() {
+                if (!canShowPopup) {
+                    return;
+                }
+                canShowPopup = false;
+
 		$('#js-stories-popup').fadeIn(250);
 		
 		//скрипт создания разметки попапа
@@ -61,11 +69,39 @@ $(document).ready(function(){
 				playPauseAutoplayFn();			
 			}
 		});
-	});	
+	}	
 	$('.js-stories-popup-close').on('click', function(){
 		$('#js-stories-popup').fadeOut(250);
 		$('#js-stories-popap-slider').slick('unslick');
+                canShowPopup = true;
 	});
+
+        if (canShowPopup) {
+                var popupTimeoutName = localStorage.getItem("popupSettingsName");
+                var popupTimeout = localStorage.getItem("popupTimeout");
+
+                if (!popupTimeout || popupTimeoutName != popupSettings.name) {
+                    popupTimeout = 0;
+                    localStorage.setItem("popupSettingsName", popupSettings.name);
+                }
+
+                var popupIntervalId = setInterval(function() {
+                    if (!canShowPopup) {
+                        return;
+                    }
+
+                    popupTimeout++;
+                    localStorage.setItem("popupTimeout", popupTimeout);
+
+                    if(jQuery.inArray(popupTimeout, popupSettings.timeouts) !== -1) {
+                         showPopup();
+                    }
+
+                    if (Math.max.apply(Math, popupSettings.timeouts) < popupTimeout) {  
+                        clearInterval(popupIntervalId);
+                    }
+                }, 1000);
+        }
 	
 	//tags bar fix
 	if($(window).width() >= 768){
